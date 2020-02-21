6cb1e1e904fc59ff1ab688d4a54467dbe7143d1c
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-02-20 22:03:20 -0500

    Added bootstrap and home.html

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import blogPost
+
+admin.site.register(blogPost)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class blogPost(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/')
+    body = models.TextField()
