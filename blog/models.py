85c9fb2e6871771048dd3ad5ee269fc536e44174
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-03-02 15:17:55 -0500

    Final first changes done

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,13 @@ class blogPost(models.Model):
     date = models.DateTimeField()
     image = models.ImageField(upload_to='images/')
     body = models.TextField()
+
+    #uses the title for the name of each blogPost object in the admin view
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def datePretty(self):
+        return self.date.strftime('%b %e %Y')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import blogPost
 
-# Create your views here.
+def allBlogs(request):
+    blogPosts = blogPost.objects
+    return render(request, 'blog/allBlogs.html', {'blogPosts':blogPosts})
+
+def detail(request, blog_id):
+    detailBlog = get_object_or_404(blogPost, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog':detailBlog})
