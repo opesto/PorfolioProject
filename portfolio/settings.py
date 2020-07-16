5759cfd3c57fefdf9d9ac0df758b2ca733f0345d
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-07-16 13:49:11 -0400

    still fixing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -58,7 +58,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, '..', 'templates')
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'sudoku', 'templates', 'sudoku')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
