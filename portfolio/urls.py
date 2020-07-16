f70555a1d467c6949ad317918765a9184b197450
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-07-16 17:06:41 -0400

    still fixing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('travel/', jobs.views.travel, name="travel"),
     path('vanBuild/', jobs.views.vanBuild, name="vanBuild"),
-    path('sudoku/', include('sudoku.urls'), name="sudoku"),
+    path('sudoku/', jobs.view.sudoku, name="sudoku"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
