66abae78c69f67b071bfe035b0d61267d68d0859
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-07-16 16:18:16 -0400

    still fixing

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from .models import blogPost
 
 def allBlogs(request):
     blogPosts = blogPost.objects
-    return render(request, 'blog/allBlogs.html', {'blogPosts':blogPosts})
+    return render(request, 'jobs/allBlogs.html', {'blogPosts':blogPosts})
 
 def detail(request, blog_id):
     detailBlog = get_object_or_404(blogPost, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog':detailBlog})
+    return render(request, 'jobs/detail.html', {'blog':detailBlog})
