21a3865f7e10c8d719f80322c44a7dd3963d59ab
Olivier Preston <olivierpreston@Oliviers-MacBook-Pro-2.local>
2020-07-16 14:08:40 -0400

    still fixing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sudoku.apps.SudokuConfig',
     'jobs.apps.JobsConfig',
     'blog.apps.BlogConfig',
-    'sudoku.apps.SudokuConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
